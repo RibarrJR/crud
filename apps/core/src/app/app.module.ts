8ec27d57f7066d432613794a65550bab417449ae
Ernesto Ribarr <j_ribarr@hotmail.com>
2021-10-20 09:37:02 -0300

    fix(estilo):organização dos estilos

--- apps/core/src/app/app.module.ts
+++ apps/core/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {SharedModule} from 'cruds-shared'
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-
+    SharedModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
